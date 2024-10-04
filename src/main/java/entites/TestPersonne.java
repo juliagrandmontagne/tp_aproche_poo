71024cbb3dae6dde0cd81091ae20c64ab0667f2c
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-04 14:59:47 +0200

    comit

--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -3,25 +3,25 @@ package entites;
 import entites2.Personne;
 
 public class TestPersonne {
-    AdressePostale adr1 = new AdressePostale(int numeroRue, String libelleRue, int codePostal, string ville);
-    adr1.numeroRue = 23;
-    adr1.libelleRue = "chapelle";
-    adr1.codePostal = 18;
-    adr1.ville = "Nice";
+    AdressePostale adr1 = new AdressePostale(numeroRue 23, libelleRue "chapelle", codePostal 12, string "ville");
+//    adr1.numeroRue = 23;
+//    adr1.libelleRue = "chapelle";
+//    adr1.codePostal = 18;
+//    adr1.ville = "Nice";
 
-    AdressePostale adr2 = new AdressePostale(int numeroRue, String libelleRue, int codePostal, string ville);
-    adr2.numeroRue = 23;
-    adr2.libelleRue = "chapelle";
-    adr2.codePostal = 18;
-    adr2.ville = "Nice"
+    AdressePostale adr2 = new AdressePostale(numeroRue 23, libelleRue "chapelle", codePostal 12, string "ville"));
+//    adr2.numeroRue = 23;
+//    adr2.libelleRue = "chapelle";
+//    adr2.codePostal = 18;
+//    adr2.ville = "Nice"
 
-    Personne id1 = new Personne(String nom, String prenom, String adresse);
-    id1.nom = "bat";
-    id1.prenom = "man";
+    Personne id1 = new Personne(nom "bat", prenom "man", new AdressePostale(numeroRue 23, libelleRue "chapelle", codePostal 12, string "ville"));
+//    id1.nom = "bat";
+//    id1.prenom = "man";
 
-    Personne id2 = new Personne(String nom, String prenom, String adresse);
-    id2.nom = "man";
-    id2.prenom = "bat";
+    Personne id2 = new Personne(nom "bat", prenom "man", new AdressePostale(numeroRue 23, libelleRue "chapelle", codePostal 12, string "ville"));
+//    id2.nom = "man";
+//    id2.prenom = "bat";
 
 
 }
