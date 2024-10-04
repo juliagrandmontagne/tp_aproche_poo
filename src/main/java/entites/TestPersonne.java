4decb409049a3504e533d2155a77c7101bab2024
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-04 11:59:32 +0200

    comit

---
+++ src/main/java/entites/TestPersonne.java
@@ -0,0 +1,27 @@
+package entites;
+
+import entites2.Personne;
+
+public class TestPersonne {
+    AdressePostale adr1 = new AdressePostale();
+    adr1.numeroRue = 23;
+    adr1.libelleRue = "chapelle";
+    adr1.codePostal = 18;
+    adr1.ville = "Nice";
+
+    AdressePostale adr2 = new AdressePostale();
+    adr2.numeroRue = 23;
+    adr2.libelleRue = "chapelle";
+    adr2.codePostal = 18;
+    adr2.ville = "Nice"
+
+    Personne id1 = new Personne();
+    id1.nom = "bat";
+    id1.prenom = "man";
+
+    Personne id2 = new Personne();
+    id2.nom = "man";
+    id2.prenom = "bat";
+
+
+}
---
+++ src/main/java/entites2/Personne.java
@@ -0,0 +1,13 @@
+package entites2;
+
+import entites.AdressePostale;
+
+public class Personne {
+    public static void main (String[]  args)
+    {
+        Public.String nom;
+        Public.String prenom;
+        Adresse postal adressePostalePersonne = new AdressePostale();
+
+    }
+}
