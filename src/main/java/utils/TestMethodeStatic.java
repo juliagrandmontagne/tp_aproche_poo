cd3c989de6d53c9f263dc90a7d6b995b6d6ad0e4
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-07 11:22:18 +0200

    tp poo

--- src/main/java/utils/TestMethodeStatic.java
+++ src/main/java/utils/TestMethodeStatic.java
@@ -3,5 +3,11 @@ package utils;
 public class TestMethodeStatic {
     public static void main(String[] args) {
         String chaine = "12";
+        int valeur = Integer.parseInt(chaine);
+        System.out.println(chaine);
+        int a = 15;
+        int b = 27;
+        int m =Integer.max(a,b );
+        System.out.println(m);
     }
 }
