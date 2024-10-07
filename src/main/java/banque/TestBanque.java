f38861e34a4c6b58abf952d1ec017f3da97ce3f7
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-07 12:12:26 +0200

    tp poo

---
+++ src/main/java/banque/TestBanque.java
@@ -0,0 +1,9 @@
+package banque;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte cmpt1 = new Compte ("18","51");
+        System.out.println(cmpt1);
+    }
+
+}
---
+++ src/main/java/banque/entites/Compte.java
@@ -0,0 +1,12 @@
+package banque.entites;
+
+public class Compte {
+    int numéroDeCompte;
+    int soldeDuCompte;
+
+    public Compte(int soldeDuCompte, int numéroDeCompte) {
+        this.soldeDuCompte = soldeDuCompte;
+        this.numéroDeCompte = numéroDeCompte;
+    }
+
+}
