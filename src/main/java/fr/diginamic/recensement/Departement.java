ac259fdf448c6494609adcee86331815270fd6c4
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-15 18:01:52 +0200

    tp auto j2

--- src/main/java/fr/diginamic/recensement/Departement.java
+++ src/main/java/fr/diginamic/recensement/Departement.java
@@ -1,5 +1,27 @@
 package fr.diginamic.recensement;
 
 public class Departement {
+    int NumeroDeDepartement;
+    int nombreTotalHabitant;
 
+    public Departement(int nombreTotalHabitant, int numeroDeDepartement) {
+        this.nombreTotalHabitant = nombreTotalHabitant;
+        NumeroDeDepartement = numeroDeDepartement;
+    }
+
+    public int getNumeroDeDepartement() {
+        return NumeroDeDepartement;
+    }
+
+    public void setNumeroDeDepartement(int numeroDeDepartement) {
+        NumeroDeDepartement = numeroDeDepartement;
+    }
+
+    public int getNombreTotalHabitant() {
+        return nombreTotalHabitant;
+    }
+
+    public void setNombreTotalHabitant(int nombreTotalHabitant) {
+        this.nombreTotalHabitant = nombreTotalHabitant;
+    }
 }
--- src/main/java/fr/diginamic/recensement/Region.java
+++ src/main/java/fr/diginamic/recensement/Region.java
@@ -1,5 +1,26 @@
 package fr.diginamic.recensement;
 public class Region {
+    int numeroDeRegion;
+    int nombreTotalHabitant;
 
+    public Region(int numeroDeRegion, int nombreTotalHabitant) {
+        this.numeroDeRegion = numeroDeRegion;
+        this.nombreTotalHabitant = nombreTotalHabitant;
+    }
 
+    public int getNumeroDeRegion() {
+        return numeroDeRegion;
+    }
+
+    public void setNumeroDeRegion(int numeroDeRegion) {
+        this.numeroDeRegion = numeroDeRegion;
+    }
+
+    public int getNombreTotalHabitant() {
+        return nombreTotalHabitant;
+    }
+
+    public void setNombreTotalHabitant(int nombreTotalHabitant) {
+        this.nombreTotalHabitant = nombreTotalHabitant;
+    }
 }
