4cdd8df9fe635b90f77a1e752b7236ed3fa25704
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-18 07:36:52 +0200

    tp autonomie j2

--- src/main/java/fr/diginamic/recensement/RechercheDepartement.java
+++ src/main/java/fr/diginamic/recensement/RechercheDepartement.java
@@ -9,28 +9,21 @@ import static fr.diginamic.recensement.Recensement.recuperationVille;
 
 public class RechercheDepartement {
     public static void rechercheDepartement () throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Tapper le code du département");
-        String departementDemander = scanner.nextLine();
-        List<Ville> V1= recuperationVille();
-        int nombretotaldhabitant = 0;
-        for (int i = 0; i < V1.size(); i++) {
-            if (V1.get(i).codeDepartement.equals(departementDemander)) {
-                nombretotaldhabitant = nombretotaldhabitant+V1.get(i).populationTotale;
-            }
-        }
-        System.out.println("nombretotaldhabitant");
-        System.out.println(nombretotaldhabitant);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Tapper le code du département");
+//        String departementDemander = scanner.nextLine();
+//        List<Ville> V1= recuperationVille();
+//        int nombretotaldhabitant = 0;
+//        for (int i = 0; i < V1.size(); i++) {
+//            if (V1.get(i).codeDepartement.equals(departementDemander)) {
+//                nombretotaldhabitant = nombretotaldhabitant+V1.get(i).populationTotale;
+//            }
+//        }
+//        System.out.println("nombretotaldhabitant");
+//        System.out.println(nombretotaldhabitant);
     }
 
-//        if (indexdemander == -1) {
-//            System.out.println("La ville demmandé n'est pas dans la liste");
-//        }
-//        else {
-//            System.out.println(V1.get(indexdemander).populationTotale);
-//        }
-
     public static void main(String[] args) throws IOException  {
-        rechercheDepartement ();
+//        rechercheDepartement ();
     }
 }
